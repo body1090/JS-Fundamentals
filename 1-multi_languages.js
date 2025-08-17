0e5cf70d64b3dbc0e18c1c617fbf537cac7235e7
shokre <bodym1207@gmail.com>
2025-08-17 17:36:30 +0300

    new file

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,3 +1,6 @@
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+const one= 'C is fun';
+const two = 'Python is cool';
+const three = 'JavaScript is amazing';
+console.log(one);
+console.log(two);
+console.log(three);
